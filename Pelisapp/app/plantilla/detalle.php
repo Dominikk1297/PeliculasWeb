b89dccc75251ec853a1dc14e28b687b8788f233a
Dominikk1297 <46744887+Dominikk1297@users.noreply.github.com>
2020-04-30 19:45:49 +0200

    Update detalle.php

--- Pelisapp/app/plantilla/detalle.php
+++ Pelisapp/app/plantilla/detalle.php
@@ -6,16 +6,17 @@ ob_start();
 $auto = $_SERVER['PHP_SELF'];
 
 ?>
+<center>
+<img src="app/img/<?=$imagen?>" height="400px" width="250px" >
 <h2> Detalles </h2>
 <table>
-<tr><td>codigo_pelicula   </td><td> <?= $clave ?></td></tr>
 <tr><td>Nombre   </td><td>   <?= $nombre ?></td></tr>
 <tr><td> director </td><td>     <?= $director ?></td></tr>
 <tr><td>genero    </td><td>    <?= $genero  ?></td></tr>
-<tr><td>img  </td><td> <img src="app/img/<?=$imagen?>" height="50px" width="50px" >  </td></tr>
-<iframe width="400" height="400" src="http://templatemonster.com/" frameborder="0"> </iframe>
+
 </table>
 <input type="button" value=" Volver " size="10" onclick="javascript:window.location='index.php'" >
+</center>
 <?php 
 
 
